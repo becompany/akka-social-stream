86191d1c4685b406f737c6fafe3df739e5853d9e
Javier Puerto <javier.puerto@becompany.ch>
2016-11-29 12:29:17 +0100

    Current cache progress.

--- src/main/scala/ch/becompany/http/HttpClient.scala
+++ src/main/scala/ch/becompany/http/HttpClient.scala
@@ -1,26 +1,34 @@
 package ch.becompany.http
 
-import java.io.IOException
+import java.io.{File, IOException}
+import java.nio.file.{Files, Path, Paths}
 
 import akka.actor.ActorSystem
+import akka.{Done, stream}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse, ResponseEntity}
-import akka.http.scaladsl.model.StatusCodes.OK
+import akka.http.scaladsl.model.{HttpHeader, HttpRequest, HttpResponse, ResponseEntity}
+import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.unmarshalling.{Unmarshal, Unmarshaller}
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{FileIO, Sink}
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Failure
 
 trait HttpClient {
 
   implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
 
-  def req[A](uri: String)(implicit unmarshaller: Unmarshaller[ResponseEntity, A], ec: ExecutionContext): Future[A] =
+  val etagCache = new scala.collection.concurrent.TrieMap[String, String]()
+  val cacheDirectory = Files.createTempDirectory("github-cache")
+
+  def req[A](uri: String, headers: scala.collection.immutable.Seq[HttpHeader])(implicit unmarshaller: Unmarshaller[ResponseEntity, A], ec: ExecutionContext): Future[A] =
     Http().singleRequest(HttpRequest(uri = uri)).
       flatMap { response =>
         response.status match {
-          case OK => Unmarshal(response.entity).to[A]
+          case OK => Unmarshal(addToCacheAndRead(uri, response)).to[A]
+          case NotModified => Unmarshal(readFromCache(uri)).to[A]
           case _ => handleError(response)
         }
       }
@@ -32,4 +40,26 @@ trait HttpClient {
     }
   }
 
+  private def addToCacheAndRead(uri: String, httpResponse: HttpResponse): ResponseEntity = {
+    httpResponse.headers.find(header => header.is("etag")) match {
+      case Some(header) => {
+        etagCache.put(uri, header.value())
+
+        httpResponse.entity.dataBytes.map(_).toMat(FileIO.toPath(getTempFilePath(header.value())))
+      }
+      case None => Unit
+    }
+    etagCache.get(uri) match {
+      case Some(etag) => readFromCache(etag)
+      case None => httpResponse.entity
+    }
+  }
+
+  private def readFromCache(uri: String): ResponseEntity = {
+    etagCache.get(uri) match {
+      case Some(etag) => FileIO.fromPath(getTempFilePath(etag)).to()
+    }
+  }
+
+  private def getTempFilePath(filename: String): Path = Paths.get(this.cacheDirectory.toString, filename)
 }
