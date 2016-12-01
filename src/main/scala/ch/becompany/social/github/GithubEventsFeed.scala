5280c9e6f021f2fc3b61ce9a48eb1f2b76f64c97
Javier Puerto <javier.puerto@becompany.ch>
2016-12-01 11:49:23 +0100

    Populate global queue with Github events every 5 minutes.

--- src/main/scala/ch/becompany/social/github/GithubEventsFeed.scala
+++ src/main/scala/ch/becompany/social/github/GithubEventsFeed.scala
@@ -1,24 +1,46 @@
 package ch.becompany.social.github
 
 import java.time.Instant
+import java.util.concurrent.TimeUnit
 
+import akka.Done
+import akka.actor.{ActorSystem, Cancellable}
 import akka.stream.OverflowStrategy
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Keep, Source}
 import ch.becompany.social.{SocialFeed, Status}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.Duration
 import scala.util.Try
 
 class GithubEventsFeed(org: String)(implicit ec: ExecutionContext) extends SocialFeed {
 
-  val limit = 30
+  var lastUpdate: Option[Instant] = None
+
+  val system = ActorSystem()
+  val updateInterval = Duration.create(5, TimeUnit.MINUTES)
 
   override def source(numLast: Int): Source[Try[Status], _] = {
+    var cancellable: Cancellable = null
     Source.queue[Try[Status]](bufferSize = 1000, OverflowStrategy.dropTail)
         .mapMaterializedValue { queue =>
-        GithubClient.events(org).foreach(_.take(numLast).foreach(status => {
-          queue.offer(Try(status))
-        }))
+          // TODO: cancel the Github client on stream termination.
+          cancellable = system.scheduler.schedule(Duration.Zero, updateInterval) {
+            GithubClient.events(org).foreach(_.sortBy(_.date).takeRight(numLast).foreach(status => {
+              lastUpdate match {
+                case Some(lastDate: Instant) => {
+                  if (lastDate.isBefore(status.date)) {
+                    lastUpdate = Some(status.date)
+                    queue.offer(Try(status))
+                  }
+                }
+                case None => {
+                  lastUpdate = Some(status.date)
+                  queue.offer(Try(status))
+                }
+              }
+            }))
+          }
       }
   }
 }
