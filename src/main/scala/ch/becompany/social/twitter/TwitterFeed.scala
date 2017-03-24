7c2c94811e6c8f5ca6515f110547060e6348e435
Javier Puerto <javier.puerto@becompany.ch>
2017-03-24 10:33:03 +0100

    Switch implementation from twitter stream to polling due to communication errors.

--- src/main/scala/ch/becompany/social/twitter/TwitterFeed.scala
+++ src/main/scala/ch/becompany/social/twitter/TwitterFeed.scala
@@ -2,12 +2,14 @@ package ch.becompany.social.twitter
 
 import java.time.Instant
 
+import akka.actor.ActorSystem
+import akka.pattern.after
 import akka.stream.scaladsl.Source
 import ch.becompany.social.{SocialFeed, Status}
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Try
+import scala.util.{Failure, Try}
 
 /**
   * Streams Twitter tweets. The continuous stream is generated based on the
@@ -20,8 +22,11 @@ import scala.util.Try
 class TwitterFeed(screenName: String, userUpdateInterval: FiniteDuration = 1 minute)(implicit ec: ExecutionContext)
   extends SocialFeed {
 
+  private val system = ActorSystem()
   private lazy val client = new TwitterClient(screenName)
-  private lazy val streamFuture = client.userId.map(id => TwitterStream(userUpdateInterval, "follow" -> id))
+  // Twitter unauthenticated requests are limited to 15 per hour. => 1 req/m
+  private val updateIntervalMin: FiniteDuration = 1 minute
+  private val updateInterval = updateIntervalMin.max(userUpdateInterval)
 
   /**
     * Returns the latest `num` Twitter tweets.
@@ -31,13 +36,34 @@ class TwitterFeed(screenName: String, userUpdateInterval: FiniteDuration = 1 min
   override def latest(num: Int): Future[List[(Instant, Status)]] =
     client.latest(num)
 
+
+  private def events(lastUpdate: Instant): Future[List[(Instant, Try[Status])]] =
+    client.latest(5).
+      map(_.
+        filter(_._1.isAfter(lastUpdate)).
+        sortBy(_._1).
+        map { case (date, status) => (date, Try(status)) }
+      ).
+      recover { case e => List(Instant.now -> Failure(e)) }
+
+  private def getLastUpdate(statuses: List[(Instant, Try[Status])]): Option[Instant] =
+    statuses.
+      lastOption.
+      map(_._1)
+
   /**
     * Streams Twitter tweets.
     * @return The source providing the stream.
     */
   override def stream(): Source[(Instant, Try[Status]), _] =
-    Source.
-      fromFuture(streamFuture).
-      flatMapConcat(_.stream)
+  Source.
+    unfoldAsync[Instant, List[(Instant, Try[Status])]](Instant.now) { lastUpdate =>
+    after(updateInterval, using = system.scheduler) {
+      events(lastUpdate) map { statuses =>
+        Some((getLastUpdate(statuses) getOrElse lastUpdate, statuses))
+      }
+    }
+  }.
+    flatMapConcat(list => Source.fromIterator(() => list.iterator))
 
 }
